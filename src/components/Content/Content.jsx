797ad48ee98db0a160343f613cadf23f86db7e33
MrBien0405 <tranbien1845@gmail.com>
2023-02-14 16:08:49 +0700

    update-14-02

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,44 +3,61 @@ import "./Content.scss";
 import CarouselSlider from "../CarouselSlider/CarouselSlider";
 import img from "../../assets/b1s.png";
 import Card from "../Card/Card.jsx";
+import TrendingTop from "../TrendingTop/TrendingTop";
 function Content() {
   return (
     <>
       <div className='ContentContainer'>
         <div className='ContentArticle'>
-          <div className="CarouselContainer">
-          <div className='textContent'>
-            <h2 className='TitleContent'>Article</h2>
-            <p className='Content'>
-              We display products based on the latest products we have, if you
-              want to see our old peoducts please enter the name of the item
-            </p>
-          </div>
-          
-            <CarouselSlider />
-         
+          <div className='CarouselContainer'>
+            <div className='textContent'>
+              <h2 className='TitleContent'>Article</h2>
+              <p className='Content'>
+                We display products based on the latest products we have, if you
+                want to see our old peoducts please enter the name of the item
+              </p>
+            </div>
 
-          <div className='BoxTip'>
-            <p className='textTip'>Tips and Trick</p>
-            <h5 className='textTitle'>
-              This 400-Square-Foot New York Apartment Is Maximized Width Custom
-              Millwork
-            </h5>
-            <div className='boxAvatar'>
-              <div className='avatarimg'>
-                <img src={img} alt='logo' />
+            <CarouselSlider />
+
+            <div className='BoxTip'>
+              <p className='textTip'>Tips and Trick</p>
+              <h5 className='textTitle'>
+                This 400-Square-Foot New York Apartment Is Maximized Width
+                Custom Millwork
+              </h5>
+              <div className='boxAvatar'>
+                <div className='avatarimg'>
+                  <img src={img} alt='logo' />
+                </div>
+                <span className='textMorgan'>By Morgan Goldberg </span>
+                <span className="ContentText">Tuesday,13 May 2023</span>
               </div>
-              <span className='textMorgan'>By Morgan Goldberg </span>
-              <span>Tuesday,13 May 2023</span>
             </div>
           </div>
+
+          <div className='CardContainer'>
+            <p className='textNew'>Daily News</p>
+            <h2 className='textTodaytop'>Today top headlines</h2>
+
+            <Card />
           </div>
 
-          <div className="CardContainer">
-            <p className="textNew">Daily News</p>
-            <h2 className="textTodaytop">Today top headlines</h2>
-            <div className="CardContainer"><Card /></div>
-            
+          <div className='TrendingTop'>
+            <p className='textTrendingTop'>Trending Topics</p>
+            <h2 className='textPopular'>Popular last week</h2>
+
+            <TrendingTop />
+          </div>
+
+          <div className='TextLoad'><p className="ptextLoad">Load More</p></div>
+
+          <div className="TextSubscribe">
+            <h2>Subscribe our newsletter</h2>
+            <div className="textTalk">
+              <p className="ptextTalk">Let's Talk</p>
+              <ion-icon className="iconTalk" name='arrow-forward-outline'></ion-icon>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/TrendingTop/TrendingTop.jsx
@@ -0,0 +1,100 @@
+import "./TrendingTop.scss";
+import img45 from "../../assets/Rectangle 45.png";
+import img46 from "../../assets/Rectangle 46.png";
+import img47 from "../../assets/Rectangle 47.png";
+import avatar from "../../assets/b1s.png";
+function TrendingTop() {
+  const dataTrendingTop = [
+    {
+      category: "Tips and Trick",
+      tittle: "Beautifull and Funcitonal Home Terrace Decoraton",
+      content:
+        "Home terrace decorations are part of every decoration or overall home design. Interiordesign.id - If by chance your house has enough space or space.",
+      img: img45,
+      avatar: avatar,
+      username: "By Morgan Goldberg",
+      date: "Friday, 1 April 2022",
+    },
+    {
+      category: "Design Inspiration",
+      tittle:
+        "Modern Minimalist Home Design: Aesthetics of Modern Home Interiors",
+      content:
+        "Home terrace decorations are part of every decoration or overall home design. Interiordesign.id – If by chance your house has enough space or space.",
+      img: img46,
+      avatar: avatar,
+      username: "By Rizal Ahmad",
+      date: "Wednesday, 17 March 2022",
+    },
+    {
+      category: "Color Guide",
+      tittle:
+        "20+ Best Kitchen Paint Colors That Make Kitchen Spaces Look More Fun",
+      content:
+        "Home terrace decorations are part of every decoration or overall home design. Interiordesign.id - If by chance your house has enough space or space.",
+      img: img47,
+      avatar: avatar,
+      username: "By Filipus Pendi",
+      date: "Saturday, 29 February 2022",
+    },
+  ];
+  return (
+    <>
+      <div className='ContainerTrendingTop'>
+        <div className='wrapperTrendingTop'>
+          <div className='boxTrendingTop1'>
+            <ul className='boxNavbarTrendingTop'>
+              <li>
+                <a className='All' href=''>
+                  All
+                </a>
+              </li>
+              <li>
+                <a href=''>Tips and Trick</a>
+              </li>
+              <li>
+                <a href=''>Interior Design</a>
+              </li>
+              <li>
+                <a href=''>Design Inspiration</a>
+              </li>
+              <li>
+                <a href=''>Color Guide</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='boxTrendingTop2'>
+            <div>
+              <ion-icon name='filter-outline'></ion-icon>
+            </div>
+            <div className='Filter'>Filter</div>
+          </div>
+        </div>
+
+        {dataTrendingTop.map((e) => (
+          <div className='wrapperTopTrending1'>
+            <div className='boxImgTop'>
+              <img src={e.img} alt='logo' />
+            </div>
+            <div className='boxTopTrending'>
+              <p className='category'>{e.category}</p>
+              <h3 className='title'>{e.tittle}</h3>
+              <p className='content'>{e.content}</p>
+
+              <div className='boxAvatar'>
+                <div className='avatarimg'>
+                  <img src={e.avatar} alt='logo' />
+                </div>
+                <span className='textMorgan'>{e.username}</span>
+                <span>{e.date}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default TrendingTop;
